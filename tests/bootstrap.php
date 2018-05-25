1343b6a8fe76c6dd932e70af59147fdc434b79fa
IvanAlekseevichPopov <ivan.alekseevich.popov@gmail.com>
2018-05-25 23:18:24 +0300

    [wip] #10: removed print

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,6 @@ $runCommand = function ($command) {
 (new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__.'/../.env');
 
 //if (isset($_ENV['BOOTSTRAP_DB'])) {
-    print_r($_ENV);
     echo 'Preparing database...'.PHP_EOL;
     $runCommand('bin/console doctrine:schema:drop --full-database --force');
     $runCommand('bin/console doctrine:migrations:migrate --no-interaction');
