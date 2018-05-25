ba8c1d4c4a9f3d8c6f6990ed39ca8a9b8322ebb7
IvanAlekseevichPopov <ivan.alekseevich.popov@gmail.com>
2018-05-25 22:54:03 +0300

    [wip] #10: added print_r

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,6 +17,7 @@ $runCommand = function ($command) {
 (new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__.'/../.env');
 
 //if (isset($_ENV['BOOTSTRAP_DB'])) {
+    print_r($_ENV);
     echo 'Preparing database...'.PHP_EOL;
     $runCommand('bin/console doctrine:schema:drop --full-database --force');
     $runCommand('bin/console doctrine:migrations:migrate --no-interaction');
