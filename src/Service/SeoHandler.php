c0653e1fd75c346ade08d1d0e7ad550efc43b919
Ivan Alekseevich Popov <ivan.alekseevich.popov@gmail.com>
2018-06-13 23:21:15 +0300

    [wip] #21: added main page

---
+++ src/Service/SeoHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Service;
+
+/**
+ * class SeoHandler
+.
+ */
+class SeoHandler
+{
+    public function test($a)
+    {
+        dump($a);
+
+        return '11122222';
+    }
+}
