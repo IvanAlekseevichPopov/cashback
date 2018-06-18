1d86ca1efa2dfe4332ea633966485dcb0fe621fa
Ivan Alekseevich Popov <ivan.alekseevich.popov@gmail.com>
2018-06-18 22:03:39 +0300

    [wip] #21: fixed tests

--- src/Service/SeoHandler.php
+++ src/Service/SeoHandler.php
@@ -1,19 +1,16 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Service;
 
 /**
- * class SeoHandler
-.
+ * class SeoHandler.
  */
 class SeoHandler
 {
     public function test($a)
     {
-        dump($a);
-
         return '11122222';
     }
 }
