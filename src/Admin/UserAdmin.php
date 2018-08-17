85bf3f2b3968d6dbe7004f06ff80f140b49ee166
ivan <ipopov@4xxi.com>
2018-08-17 23:55:14 +0300

    [template] #23: updated vendors

--- src/Admin/UserAdmin.php
+++ src/Admin/UserAdmin.php
@@ -4,16 +4,39 @@ declare(strict_types=1);
 
 namespace App\Admin;
 
+use App\Entity\User;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 /**
  * Class UserAdmin.
  */
 class UserAdmin extends AbstractAdmin
 {
+    /**
+     * @param object $object
+     */
+    public function prePersist($object)
+    {
+        /* @var User $object */
+        parent::prePersist($object);
+        $this->updatePassword($object);
+    }
+
+    /**
+     * @param object $object
+     */
+    public function preUpdate($object)
+    {
+        /* @var User $object */
+        parent::preUpdate($object);
+        $this->updatePassword($object);
+    }
+
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
@@ -24,50 +47,64 @@ class UserAdmin extends AbstractAdmin
             ->add('roles');
     }
 
-    protected function configureFormFields(FormMapper $form)
+    public function configureFormFields(FormMapper $form)
     {
         $form
-            ->add(
-                'id',
-                null,
-                [
-                    'attr' => [
-                        'readonly' => true,
-                    ],
-                ]
-            )
+            ->add('id', null, [
+                'attr' => ['readonly' => true,],
+            ])
+            ->add('username')
             ->add('email')
+            ->add('plainPassword', TextType::class, [
+                'required' => false,
+            ])
             ->add('enabled')
-            ->add(
-                'roles',
-                ChoiceType::class,
-                [
-                    'choices' => $this->getRoles(),
-                    'multiple' => true,
-                ]
-            );
+            ->add('roles', ChoiceType::class, [
+                'choices' => $this->getRoles(),
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    /**
+     * @param DatagridMapper $filter
+     */
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+            ->add('username')
+            ->add('email')
+            ->add('enabled');
     }
 
     /**
      * @return array
      */
-    protected function getRoles()
+    private function getRoles()
     {
         $rolesHierarchy = $this->getConfigurationPool()->getContainer()->getParameter('security.role_hierarchy.roles');
 
         $flatRoles = [];
-        foreach ($rolesHierarchy as $roles) {
+        foreach ($rolesHierarchy as $parent => $roles) {
             if (empty($roles)) {
                 continue;
             }
 
             foreach ($roles as $role) {
-                if (!isset($flatRoles[$role])) {
-                    $flatRoles[$role] = $role;
-                }
+                $flatRoles[$role] = $role;
             }
         }
 
         return $flatRoles;
+    }
+
+    /**
+     * @param User $user
+     */
+    private function updatePassword(User $user)
+    {
+        if ($user->getPlainPassword()) {
+            $this->getConfigurationPool()->getContainer()->get('fos_user.user_manager')->updateUser($user, false);
+        }
     }
 }
--- src/Migrations/Version20180414163052.php
+++ src/Migrations/Version20180414163052.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Version20180414163052.
