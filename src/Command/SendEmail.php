88286f6c2f27ad6453509620ca21783635a4122c
IvanAlekseevichPopov <ivan.alekseevich.popov@gmail.com>
2018-08-29 21:07:27 +0300

    [mail] #30: added guzzle test request for mail

--- src/Command/SendEmail.php
+++ src/Command/SendEmail.php
@@ -1,21 +1,10 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Command;
 
-use App\DBAL\Types\Enum\TransactionEnumType;
-use App\DBAL\Types\Enum\TransactionStatusEnumType;
-use App\Entity\CashBack;
-use App\Entity\CashBackPlatform;
-use App\Entity\CashBackTrek;
-use App\Entity\Transaction;
-use App\Manager\TransactionManager;
-use App\Repository\CashBackRepository;
-use App\Service\AdmitadApiHandler;
-use Doctrine\ORM\EntityManagerInterface;
-use Mailgun\Mailgun;
-use Monolog\Logger;
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -42,15 +31,44 @@ class SendEmail extends ContainerAwareCommand
     {
         $output->writeln('START');
 
-        $mgClient = new Mailgun('ae06c3a8d6d17bdb53f8d428769604b1-a4502f89-3beb2a14');
-        $domain = "cashtan.tk";
+//        $mgClient = new Mailgun('ae06c3a8d6d17bdb53f8d428769604b1-a4502f89-3beb2a14');
+//        $domain = "cashtan.tk";
+//        $result = $mgClient->sendMessage($domain, array(
+//            'from'    => 'Excited User <mailgun@cashtan.tk>',
+//            'to'      => 'tirblabla@gmail.com',
+//            'subject' => 'Hello',
+//            'text'    => 'Testing some Mailgun awesomness!'
+//        ));
 
-        $result = $mgClient->sendMessage($domain, array(
-            'from'    => 'Excited User <mailgun@cashtan.tk>',
-            'to'      => 'tirblabla@gmail.com',
-            'subject' => 'Hello',
-            'text'    => 'Testing some Mailgun awesomness!'
-        ));
+
+        $client = new Client(['base_uri' => 'https://api.mailgun.net']);
+//
+        $res = $client->request('POST', '/v3/cashtan.tk/messages', [
+            'auth' => [
+                'api',
+                'ae06c3a8d6d17bdb53f8d428769604b1-a4502f89-3beb2a14',
+            ],
+            'multipart' => [
+                [
+                    'name' => 'from',
+                    'contents' => 'Excited User <mailgun@cashtan.tk>',
+                ],
+                [
+                    'name' => 'to',
+                    'contents' => 'tirblabla@gmail.com',
+                ],
+                [
+                    'name' => 'subject',
+                    'contents' => 'Hello',
+                ],
+                [
+                    'name' => 'text',
+                    'contents' => '2Testing some Mailgun awesomeness2',
+                ],
+            ],
+        ]);
+
+        $output->writeln($res->getBody()->getContents());
 
         $output->writeln('STOP');
     }
